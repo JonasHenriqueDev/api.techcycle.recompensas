7101c1d8d943a045ab60588c45e24392d072f343
Jonas Santos <jonashenrique965@gmail.com>
2024-07-01 23:50:31 -0300

    init

---
+++ src/test/java/br/upe/api/techcycle/recompensas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.api.techcycle.recompensas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
